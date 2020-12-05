099f9f7bc9846d38c953d342bd1ee201598fa039
Vasyl Boyko <vboiko0@gmail.com>
2020-12-05 22:01:38 +0200

    add: elements to placeholder

--- src/components/AddWebsiteModal.vue
+++ src/components/AddWebsiteModal.vue
@@ -6,7 +6,7 @@
     <div
       class="absolute w-full h-full bg-gray-900 opacity-50 top-0 left-0 flex items-center justify-center"
     ></div>
-    <div class="w-1/3 h-1/2 bg-white text-indigo-500 relative">
+    <div class="w-96 h-1/2 bg-white text-indigo-500 relative">
       <button
         class="text-indigo-500 flex justify-center mt-4 mr-4 absolute top-0 right-0 focus:outline-none"
         @click="onClose"
@@ -22,18 +22,56 @@
           />
         </svg>
       </button>
+
+      <div class="mx-12 mt-10 mb-6">
+        <h2 class="font-black text-3xl text-indigo-700">Add website</h2>
+        <text-input
+          class="mt-4"
+          v-model="websiteUrl"
+          label="Website url"
+          placeholder="wordproof.io"
+        />
+        <cms-select class="mt-4" v-model="cms" />
+        <dropdown
+          class="mt-6"
+          :options="blockchainOptions"
+          placeholder="Select the blockchain"
+        />
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import Dropdown from "@/components/shared/Dropdown.vue";
+import TextInput from "@/components/shared/TextInput.vue";
+import CmsSelect from "@/components/shared/CmsSelect.vue";
+import { CMSType } from "@/types";
 
 export default Vue.extend({
   name: "AddWebsiteModal",
 
+  components: {
+    Dropdown,
+    TextInput,
+    CmsSelect,
+  },
+
   props: {
     show: { type: Boolean, required: true },
+  },
+
+  data() {
+    return {
+      blockchainOptions: [
+        { value: 0, label: "EOS (recommended)" },
+        { value: 1, label: "Telos" },
+        { value: 2, label: "Ethereum" },
+      ],
+      websiteUrl: "",
+      cms: "",
+    };
   },
 
   methods: {
