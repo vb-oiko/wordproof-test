be6c6ddc0f6de5aaa8b2d7b46f0f8ca1941eeef3
Vasyl Boyko <vboiko0@gmail.com>
2020-12-05 18:43:46 +0200

    add: text input component

---
+++ src/components/shared/TextInput.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="border border-gray-800 rounded relative p-2">
+    <label
+      for="price"
+      class="block text-sm font-medium text-indigo-700 bg-white px-2 absolute -top-3 left-3"
+      >{{ label }}</label
+    >
+    <input
+      type="text"
+      name="price"
+      id="price"
+      class="px-2 pb-1 pt-2 block w-full focus:outline-none text-gray-800"
+      placeholder="0.00"
+      v-model="inputValue"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "TextInput",
+
+  props: {
+    value: { type: String, required: true },
+    label: { type: String, default: "" },
+    placeholder: { type: String, default: "" },
+  },
+
+  data() {
+    return {
+      inputValue: this.value,
+    };
+  },
+});
+</script>
