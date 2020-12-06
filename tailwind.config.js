cf7cde5e5d883302908429fef0067022a33a0931
Vasyl Boyko <vboiko0@gmail.com>
2020-12-06 14:29:36 +0200

    fix: tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ["./components/**/*.vue", "./views/**/*.vue"],
+  purge: ["./src/**/*.vue"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
