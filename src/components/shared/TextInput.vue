39d01ba680033debba89aace3bfe4f2611311205
Vasyl Boyko <vboiko0@gmail.com>
2020-12-06 13:04:02 +0200

    add: unique id and name for input

--- src/components/shared/TextInput.vue
+++ src/components/shared/TextInput.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="border border-gray-800 rounded relative p-2">
     <label
-      for="price"
+      :for="id"
       class="block text-sm font-medium text-indigo-700 bg-white px-2 absolute -top-3 left-3"
       >{{ label }}</label
     >
     <input
       type="text"
-      name="price"
-      id="price"
+      :name="id"
+      :id="id"
       class="px-2 pb-1 pt-2 block w-full focus:outline-none text-gray-800"
       :placeholder="placeholder"
       v-model="inputValue"
+      @input="$emit('input', inputValue)"
     />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import { uniqueLabel } from "@/helpers";
 
 export default Vue.extend({
   name: "TextInput",
@@ -31,6 +33,7 @@ export default Vue.extend({
   data() {
     return {
       inputValue: this.value,
+      id: uniqueLabel(this.label),
     };
   },
 });
